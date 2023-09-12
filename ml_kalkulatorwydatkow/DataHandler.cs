460dcd589c9d1b5ff4e692d5173949de2c5ecef5
Guru18650 <michaletk@outlook.com>
2023-09-12 12:13:15 +0200

    some stuff idk

---
+++ ml_kalkulatorwydatkow/DataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+namespace ml_kalkulatorwydatkow
+{
+    internal class DataHandler
+    {
+        static string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "db.db3");
+
+        SQLiteConnection db = new SQLiteConnection(dbPath);
+
+    }
+}
